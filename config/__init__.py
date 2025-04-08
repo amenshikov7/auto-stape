c5333f6039a68bf2701e334938a212ad3777f18a
Alexander <alex@MacBook-Pro.local>
2025-04-08 12:07:48 +0500

    add first autotest

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,6 @@
 from config.url import Url
 from config.playwright import Playwright
-form config.expectations import Expectations
+from config.expectations import Expectations
 
 url = Url()
 playwright = Playwright()
